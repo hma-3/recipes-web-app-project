b09854633563638069d8368e69c9adfbd036a3a1
Mariia <maria.gula0307@gmail.com>
2025-01-21 10:38:35 +0200

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,48 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      'implicit-arrow-linebreak': 0,
+      'import/prefer-default-export': 0,
+      'max-len': [
+        'error',
+        {
+          ignoreTemplateLiterals: true,
+          ignoreComments: true,
+        },
+      ],
+      'no-console': 'warn',
+      'no-param-reassign': [2, { props: true }],
+      'react/react-in-jsx-scope': 0,
+      'react/destructuring-assignment': 0,
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          parser: 'flow',
+        },
+      ],
+    },
+  },
 ];
-
-export default eslintConfig;
